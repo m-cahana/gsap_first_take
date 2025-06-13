bfc768560044a57ad0b45478ca831ebb71175587
Michael Cahana <cahanamichael@gmail.com>
2025-06-12 22:07:00 -0400

    infinity

--- rainbow.js
+++ rainbow.js
@@ -5,25 +5,29 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 const content = document.querySelector("#content");
 
 /*------------------------------
-Making some circles noise
+Making some infinity SVGs noise
 ------------------------------*/
 const simplex = new SimplexNoise();
 for (let i = 0; i < 5000; i++) {
-  const div = document.createElement("div");
-  div.classList.add("circle");
+  const img = document.createElement("img");
+  img.src = "infinity.svg";
+  img.classList.add("infinity");
   const n1 = simplex.noise2D(i * 0.003, i * 0.0033);
   const n2 = simplex.noise2D(i * 0.002, i * 0.001);
 
   const style = {
-    transform: `translate(${n2 * 200}px) rotate(${n2 * 270}deg) scale(${
-      3 + n1 * 2
-    }, ${3 + n2 * 2})`,
-    boxShadow: `0 0 0 .2px hsla(${Math.floor(i * 0.3)}, 70%, 70%, .6)`,
+    transform: `translate(${n2 * 200}px, ${n1 * 200}px) rotate(${
+      n2 * 270
+    }deg) scale(${3 + n1 * 2}, ${3 + n2 * 2})`,
+    opacity: 0,
+    position: "absolute",
+    width: "20px",
+    height: "20px",
   };
-  Object.assign(div.style, style);
-  content.appendChild(div);
+  Object.assign(img.style, style);
+  content.appendChild(img);
 }
-const Circles = document.querySelectorAll(".circle");
+const Infinities = document.querySelectorAll(".infinity");
 
 /*------------------------------
 Init ScrollSmoother
@@ -45,8 +49,8 @@ const main = gsap.timeline({
     end: "bottom bottom",
   },
 });
-Circles.forEach((circle) => {
-  main.to(circle, {
-    opacity: 1,
+Infinities.forEach((infinity) => {
+  main.to(infinity, {
+    opacity: 0.6,
   });
 });
